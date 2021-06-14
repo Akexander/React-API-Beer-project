ac6bd1b0949a649f2e7d9b1c920ee2d43782918e
Akexander <alexanderfazan@gmail.com>
2021-06-14 21:52:16 +0100

    Addition of some text

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+
+
+function App() {
+  const [beers, updateBeers] = useState([]);
+  const [submitSearch, setSubmitSearch] = useState("");
+  
+  useEffect(() => {
+    const api_Url = "https://api.punkapi.com/v2/beers";
+    fetch(api_Url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => updateBeers(data));
+  }, []);
+
+  const handleChange = e => {
+    setSubmitSearch(e.target.value)
+  }
+  let filteredBeers = []
+  if(beers.length > 0) {
+    filteredBeers = beers.filter(beers => 
+
+      beers.name.toLowerCase().includes(submitSearch.toLowerCase())
+      )
+  }
+  
+
+    return (
+      <>
+      <section className="header">
+      <h1>Beers Online</h1>
+      <form className="beerSearchInput">
+        <input type="text" placeholder="Choose your beer..." className="submitSearch" onChange={handleChange} />
+      </form>
+      </section>
+      
+
+        {filteredBeers.map(beers => {
+        return (
+          <section className="beerCard">
+          
+          <section className="text-copy">
+          <h2>{beers.name} </h2>
+           <h3>{beers.tagline}</h3>
+          <br />
+          <p>{beers.description}</p>
+          <br />
+          <h3>ABV:{beers.abv}</h3>
+          </section>
+          <img className="image" src="{beers.image_url}" alt="image" />
+          </section>
+        );
+        })}
+        
+      </>
+      )
+}
+
+export default App;
