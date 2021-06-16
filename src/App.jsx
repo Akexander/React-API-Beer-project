f613f022fe5afe764825500d7ba3dd2ed8452241
Akexander <alexanderfazan@gmail.com>
2021-06-16 07:50:12 +0100

    Added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-
-
+import logo1 from "./images/logo1.png";
+import logo2 from "./images/logo2.png";
 
 function App() {
   const [beers, updateBeers] = useState([]);
   const [submitSearch, setSubmitSearch] = useState("");
-  
+
   useEffect(() => {
     const api_Url = "https://api.punkapi.com/v2/beers";
     fetch(api_Url)
@@ -16,50 +16,50 @@ function App() {
       .then((data) => updateBeers(data));
   }, []);
 
-  const handleChange = e => {
-    setSubmitSearch(e.target.value)
-  }
-  let filteredBeers = []
-  if(beers.length > 0) {
-    filteredBeers = beers.filter(beers => 
-
+  const handleChange = (e) => {
+    setSubmitSearch(e.target.value);
+  };
+  let filteredBeers = [];
+  if (beers.length > 0) {
+    filteredBeers = beers.filter((beers) =>
       beers.name.toLowerCase().includes(submitSearch.toLowerCase())
-      )
+    );
   }
-  
 
-    return (
-      <>
+  return (
+    <>
       <section className="header">
-      <h1>Beers Online</h1>
-     
+        <div>
+          <img src={logo2} alt="" />
+        </div>
 
-      
-      <form className="beerSearchInput">
-        <input type="text" placeholder="Choose your beer..." className="submitSearch" onChange={handleChange} />
-      </form>
+        <form className="beerSearchInput">
+          <input
+            type="text"
+            placeholder="Choose your beer..."
+            className="submitSearch"
+            onChange={handleChange}
+          />
+        </form>
       </section>
-      
 
-        {filteredBeers.map(beers => {
+      {filteredBeers.map((beers) => {
         return (
           <section className="beerCard">
-          
-          <section className="text-copy">
-          <h2>{beers.name} </h2>
-           <h3>{beers.tagline}</h3>
-          <br />
-          <p>{beers.description}</p>
-          <br />
-          <h3>ABV:{beers.abv}</h3>
-          </section>
-          <img className="image" src={beers.image_url} alt="image" />
+            <section className="text-copy">
+              <h2>{beers.name} </h2>
+              <h3>{beers.tagline}</h3>
+              <br />
+              <p>{beers.description}</p>
+              <br />
+              <h3>ABV:{beers.abv}</h3>
+            </section>
+            <img className="image" src={beers.image_url} alt="image" />
           </section>
         );
-        })}
-        
-      </>
-      )
+      })}
+    </>
+  );
 }
 
 export default App;
